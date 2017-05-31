7ad57dceff59d39de971bd40cbd594805dc7efb4
lukasheinrich <lukas.heinrich@gmail.com>
2017-05-31 04:22:40 +0200

    transitioning to logstash

--- recastcelery/messaging.py
+++ recastcelery/messaging.py
@@ -3,7 +3,6 @@ import emitter
 import logging
 import json
 import redis
-import logging
 import os
 
 log = logging.getLogger(__name__)
@@ -45,6 +44,27 @@ class RecastLogger(logging.StreamHandler):
         msg = self.format(record)
         socketlog(self.jobid,msg)
 
+class RedisHandler(logging.StreamHandler):
+    def __init__(self,jobguid):
+        self.red = redis.StrictRedis(
+            host = os.environ.get('RECAST_CELERY_REDIS_HOST','localhost'),
+            port = os.environ.get('RECAST_CELERY_REDIS_DB',6379),
+            db = os.environ.get('RECAST_CELERY_REDIS_PORT',0)
+        )
+        self.jobguid = jobguid
+        logging.StreamHandler.__init__(self)
+
+    def emit(self, record):
+        msg = self.format(record)
+        data = {
+            'wflowguid': self.jobguid,
+            'msg_type':'wflow_log', #__ for logstash
+            'msg':msg,
+            'date':datetime.now().strftime('%Y-%m-%d %X'),
+            'type':'log_message'
+        }
+        self.red.publish(os.environ.get('PACKTIVITY_LOGGER_CHANNEL','logstash:in'),json.dumps(data))
+
 def get_stored_messages(jobguid):
     msglist = jobguid_message_key(jobguid)
     red = get_redis()
@@ -57,4 +77,9 @@ def setupLogging(jobguid):
     recastlogger.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
     log.setLevel(logging.INFO)
     log.addHandler(recastlogger)
+
+    redishandler = RedisHandler(jobguid)
+    log.addHandler(redishandler)
+
+
     return log,recastlogger
