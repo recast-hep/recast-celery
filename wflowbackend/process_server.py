22496a43a827f3443c0acab411093c99ae98b04d
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-12 18:10:28 +0100

    restrucuture process.py

--- wflowbackend/process_server.py
+++ wflowbackend/process_server.py
@@ -1,27 +1,44 @@
 import json
 import argparse
 import logging
+import os
+
+import wflowbackend.backendtasks as backendtasks
+import wflowbackend.messaging as messaging
 from flask import Flask, request, jsonify
 log = logging.getLogger('process_server')
-
 
 app = Flask('process_server')
 app.debug = True
 
+def get_context():
+    return json.load(open(app.config['context_file']))
 
-@app.route('/setup')
-def setup():
-    log.info('setting up')
-    return '{} {}'.format(app.config['wflowid'], app.config['setupfunc'])
+@app.route('/context')
+def context():
+    return jsonify(get_context())
+
+@app.route('/finalize')
+def finalize():
+    log.info('finalizing')
+    ctx = get_context()
+    teardownfunc = getattr(backendtasks,app.config['successfunc'])
+    teardownfunc(ctx)
+    log.info('successfunc done')
+    return jsonify({'status': 'ok'})
 
 @app.route('/teardown')
 def teardown():
     log.info('tearing down')
-    return '{} {}'.format(app.config['wflowid'], app.config['teardownfunc'])
+    ctx = get_context()
+    teardownfunc = getattr(backendtasks,app.config['teardownfunc'])
+    teardownfunc(ctx)
+    log.info('teardown done')
+    return jsonify({'status': 'ok'})
 
 @app.route('/status', methods = ['GET','POST'])
 def status():
-    statusfile = app.config['statusfile']
+    statusfile = app.config['status_file']
     if request.method == 'POST':
         status_data = request.json
         status_data = {'success': status_data['success'], 'ready': status_data['ready']}
@@ -41,6 +58,26 @@ def status():
                 json.dump(data, f)
                 return jsonify(data)
 
+def init():
+    log.info('setting up')
+
+    ctx = backendtasks.acquire_context(app.config['wflowid'])
+    setupfunc = getattr(backendtasks,app.config['setupfunc'])
+    setupfunc(ctx)
+
+    CONTEXTFILE = '.wflow_context'
+    STATUSFILE = '.wflow_status'
+
+    app.config['context_file'] = os.path.join(ctx['workdir'],CONTEXTFILE)
+    app.config['status_file'] = os.path.join(ctx['workdir'],STATUSFILE)
+    log.info('declaring context and status files at %s %s',
+             app.config['context_file'], app.config['status_file'])
+
+    with open(app.config['context_file'],'w') as f:
+        json.dump(ctx,f)
+
+    log.info('setup done')
+
 def main():
     logging.basicConfig(level = logging.INFO)
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -48,11 +85,16 @@ def main():
     parser.add_argument('successfunc', metavar='successfunc', help='sucess exit function')
     parser.add_argument('teardownfunc', metavar='teardownfunc', help='exit/cleanup function (always called)')
     parser.add_argument('wflowid', metavar='wflowid', help='workflow id')
+    parser.add_argument('--stream-logs', dest='stream_logs',  action="store_true", help='stream logging')
+
     args = parser.parse_args()
-    app.config['statusfile'] = 'wflow_status.json'
     app.config['wflowid'] = args.wflowid
     app.config['setupfunc'] = args.setupfunc
     app.config['successfunc'] = args.successfunc
     app.config['teardownfunc'] = args.teardownfunc
     log.info('starting server')
+
+    messaging.setupLogging(args.wflowid, add_redis = args.stream_logs)
+
+    init()
     app.run(host='0.0.0.0', port=5000)
